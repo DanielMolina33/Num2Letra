d070c447aa59c3a3d870d2bcab6654fde3adf1f3
Jose Daniel Molina <josedanielmolina323@gmail.com>
2025-12-22 18:13:59 -0500

    environment variables were set

--- sett.py
+++ sett.py
@@ -1,5 +1,7 @@
+import os
+
 token = 'Num2Letra'
-whatsapp_token = 'EAA7YRmFZC9lsBQfcAB7ZAiAToWwkzRTU7hAah40ZAXwxGfiS56e7sAH94FLcG7C3fjHbJg0lOpTJZBcQKwg9wquD0LYjyJ5e5RvZAdgxyx41oegtyFwgF2TywA6eiwGADJfBWXiGTmgk3YfOJ1yLw1TZA2rF1U8Yg6x7lzr9QdfjyUZAw9QpSFhOXnO9K7vkPILzgZDZD'
+whatsapp_token = os.environ['WPP_TOKEN']
 whatsapp_url = 'https://graph.facebook.com/v22.0/844939012046578/messages'
 
 stickers = {
